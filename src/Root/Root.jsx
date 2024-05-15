ea6a4d9c9ee6d80962044ea960d506cbf4c243b6
smsaifurrahman <smsaifur525@gmail.com>
2024-05-15 08:30:44 +0200

    Submission Completed

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -6,7 +6,7 @@ import Footer from '../Components/Footer';
 
 const Root = () => {
     return (
-        <div className='my-10 max-w-7xl mx-auto px-2 md:px-2 lg:px-0 '>
+        <div className='mt-4 max-w-7xl mx-auto px-2 md:px-2 lg:px-0 '>
           {/* Navbar */}
           <Navbar></Navbar>
          <div className='min-h-screen'>
